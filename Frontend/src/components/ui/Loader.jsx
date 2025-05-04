3255c8cc28267b64313f04d17be17aff4a310b94
Rakindu Marambe <116582517+RakinduM@users.noreply.github.com>
2025-05-04 23:23:26 +0530

    minor changes 2

--- Frontend/src/components/ui/Loader.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Loader = () => {
-  return <p className="text-blue-500">Loading<span className="loading loading-dots loading-xs"></span></p>;
-};
-
-export default Loader;
